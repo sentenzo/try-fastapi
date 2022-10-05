97ea85b809c6f07ca24cbf58a71cdbb23f9e25af
sentenzo <kassiyskley@rambler.ru>
2022-10-05 14:16:30 +0300

    primitive CDN functionality

--- tryFastAPI/main.py
+++ tryFastAPI/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+HTTP_STORAGE = {}
+
+
+@app.put("/set/http")
+async def set_http(key: str, http: str) -> None:
+    HTTP_STORAGE[key] = http
+
+
+@app.get("/get/http/{key}", response_class=HTMLResponse)
+async def get_http(key: str) -> str:
+    html = f"<p>Key not found: {key}</p>"
+    return HTTP_STORAGE.get(key, html)
