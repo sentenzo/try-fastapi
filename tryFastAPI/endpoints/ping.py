55eb9b499e15586db2a3dc6c7867a998b4b49ce7
sentenzo <sentenzo@users.noreply.github.com>
2022-10-31 20:57:02 +0300

    testing GuitHub actions

--- tryFastAPI/endpoints/ping.py
+++ tryFastAPI/endpoints/ping.py
@@ -7,18 +7,18 @@ from tryFastAPI.schemas import SimpleResponce
 from tryFastAPI.db.connection.session import get_session_dependency as get_session
 
 api_router = APIRouter(
-    prefix="/ping",
-    tags=["Application Health"],
+    prefix = "/ping",
+    tags = ["Application Health"],
 )
 
 
-@api_router.get("/", response_model=SimpleResponce, status_code=status.HTTP_200_OK)
-@api_router.get("/app", response_model=SimpleResponce, status_code=status.HTTP_200_OK)
+@api_router.get("/", response_model = SimpleResponce, status_code = status.HTTP_200_OK)
+@api_router.get("/app", response_model = SimpleResponce, status_code = status.HTTP_200_OK)
 async def ping_app():
     return SimpleResponce(message="Application responds")
 
 
-@api_router.get("/db", response_model=SimpleResponce, status_code=status.HTTP_200_OK)
+@api_router.get("/db", response_model = SimpleResponce, status_code = status.HTTP_200_OK)
 async def ping_db(db: Session = Depends(get_session)):
     result = await db.scalar(select(text("1")))
     if result:
