3cee2a7bb96d358311d34516cd30fe7bda5c7c07
sentenzo <kassiyskley@rambler.ru>
2022-11-06 14:21:38 +0300

    step

--- service/__main__.py
+++ service/__main__.py
@@ -1,7 +1,7 @@
 from random import randrange
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel, Field
 
 
@@ -34,16 +34,42 @@ async def root():
     return {"message": "root"}
 
 
-@app.get("/post")
+@app.get("/post/all")
 async def get_posts():
     return {"data": db.post}
 
 
-@app.post("/post")
+@app.get("/post/{id}")
+async def get_post(id: int, responce: Response):
+    if not id in db.post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No post with id={id}")
+    else:
+        return {"data": db.post[id]}
+
+
+@app.post("/post", status_code=status.HTTP_201_CREATED)
 async def create_post(new_post: Post):
     db.add_post(new_post)
     print(db.post)
     return new_post
+
+
+@app.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    if not id in db.post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No post with id={id}")
+    else:
+        del db.post[id]
+
+
+@app.put("/post/{id}")
+async def update_post(id: int, updated_post: Post):
+    if not id in db.post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No post with id={id}")
+    else:
+        db.post[id] = updated_post
+        db.post[id].id = id
+        return {"data": db.post[id]}
 
 
 if __name__ == "__main__":
